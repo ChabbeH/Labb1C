64fcf74836222b5d15e5c1e2d9db2ed0be1e40cf
ChabbeH <72022949+ChabbeH@users.noreply.github.com>
2021-12-15 16:51:35 +0100

    Labb1C

---
+++ Labb1/Dog.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Labb1
+{
+    internal class Dog : Animal
+    {
+        public Dog(string name, int id) : base(name, id)
+        {
+
+
+        }
+      
+    }
+
+}
